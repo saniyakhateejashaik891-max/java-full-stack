ed0f91bb635ab0cf30e81820e4e2434adb1c2a2b
saniyakhateejashaik891-max <saniyakhateejashaik891@gmail.com>
2026-03-12 14:04:08 +0530

    Update hashMap.java

--- day6/hashMap.java
+++ day6/hashMap.java
@@ -4,13 +4,13 @@ class Hash{
     public static void main(String[] args) {
         
         HashMap<String,Integer> map=new HashMap<>();
-        map.put("Mamta",1 );
-        map.put("Lalita  ",2);
-        map.put("Veena", 3);
-        map.put("Indhira", 4);
+        map.put("Saboor",1 );
+        map.put("sidra",2);
+        map.put("Hania", 3);
+        map.put("yumna", 4);
 
-        System.out.println(map.containsKey("Reshma"));
-        map.get("Veena");
+        System.out.println(map.containsKey("Inayat"));
+        map.get("Hania");
         
         //Iteration
         for(Map.Entry<String,Integer> e:map.entrySet()){
